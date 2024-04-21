d33ff438a438d868cf0eba3cfe6a05cd085b5b08
auh <21078607+fanlushuai@users.noreply.github.com>
2024-04-21 23:04:22 +0800

    f

--- robot.js
+++ robot.js
@@ -106,6 +106,7 @@ const Robot = {
 
     let tryCount = 0;
 
+    let useComment = Config.useComment;
     if (useComment != null && useComment != "") {
       log("使用指定评论 %s", useComment);
       hotComment = useComment;
