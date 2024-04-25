2496fcbfa06b50dbc3d19efb26b4d39c3187d1ef
fanlushuai <21078607+fanlushuai@users.noreply.github.com>
2024-04-25 23:34:13 +0800

    完善ks动作

--- ks.js
+++ ks.js
@@ -1,5 +1,142 @@
-const ks = {
-    name: "ks"
-    
+const { AutojsUtil } = require("./autojsUtil");
+
+const KS = {
+    name: "快手",
+    intoSearch: function () {
+        log("搜索tab")
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id('search_btn').visibleToUser(true),
+            "搜索tab",
+            8,
+            this.name
+        );
+    },
+    inputKeyWord: function (keyWord) {
+        let ele = AutojsUtil.getEleBySelectorWithAutoRefresh(
+            id("editor").visibleToUser(true),
+            "搜索输入框",
+            8,
+            this.name
+        );
+        if (ele) {
+            log("输入关键字 %s", keyWord);
+            ele.setText(keyWord);
+        }
+    }, search: function () {
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id('right_button').visibleToUser(true),
+            "搜索",
+            8,
+            this.name
+        );
+    },
+    filterTab: function () {
+        log("筛选tab");
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id('tab_filter_image').visibleToUser(true),
+            "筛选tab",
+            8,
+            this.name
+        );
+    },
+    filterMostStar: function () {
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            text("点赞最多").visibleToUser(true),
+            "筛选most star",
+            8,
+            this.name
+        );
+
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("confirm_btn").visibleToUser(true),
+            "确定",
+            8,
+            this.name
+        );
+    },
+    intoVedio: function () {
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("subject_op_title_newfeed").visibleToUser(true),
+            "进视频",
+            8,
+            this.name
+        );
+    },
+    commentTab: function () {
+
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("comment_icon").visibleToUser(true),
+            "评论Tab",
+            8,
+            this.name
+        );
+    },
+    getHotComment: function () {
+        // 置顶不好处理，
+        // 作者也不好排除
+        // 那么使用点赞量，来决定，哪个评论更有价值
+        let likeEls = id("tv_like_count").find()
+        let maxCountLikeEle
+        let maxCount = 0
+        for (let e of likeEls) {
+            if (parseInt(e.getText()) > maxCount) {
+                maxCount = parseInt(e.getText())
+                maxCountLikeEle = e
+            }
+        }
+
+        let hotComment = maxCountLikeEle.parent().parent().parent().findOne(id('comment')).getText()
+        log('热评->', hotComment)
+        return hotComment;
+    }, closeCommentTab: function () {
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("tabs_panel_close").visibleToUser(true),
+            "关闭评论Tab",
+            8,
+            this.name
+        );
+    }, comment: function (comment) {
+
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("editor_holder_text").visibleToUser(true),
+            "点击评论框",
+            8,
+            this.name
+        );
+
+        let ele = AutojsUtil.getEleBySelectorWithAutoRefresh(
+            id("editor").visibleToUser(true),
+            "评论输入框",
+            8,
+            this.name
+        );
+
+        log("设置评论 %s", comment)
+        ele.setText(comment)
+
+        sleep(500)
+
+
+        AutojsUtil.clickSelectorWithAutoRefresh(
+            id("finish_button").visibleToUser(true),
+            "发送",
+            8,
+            this.name
+        );
+
+    }
+
 
 }
+
+// KS.comment(KS.getHotComment())
+// KS.filterTab();
+// KS.filterMostStar();
+// KS.intoVedio();
+// KS.commentTab();
+// KS.getHotComment();
+// KS.closeCommentTab();
+
+// module.exports = {
+//     KS
+// }
--- test.js
+++ test.js
@@ -6,6 +6,9 @@
 // // .parent().parent().parent().find(className("ImageView"))
 // log("%j", b)
 
+const { AutojsUtil } = require("./autojsUtil")
+const { KS } = require("./ks")
+
 
 
 
@@ -13,5 +16,80 @@
 
 // id("com.ss.android.ugc.aweme:id/tv_desc").findOne().click()
 
-let eles=className("ImageView").visibleToUser(true).find()
-log(eles.size())
+// let eles=className("ImageView").visibleToUser(true).find()
+// log(eles.size())
+
+
+// let ele = AutojsUtil.getEleBySelectorWithAutoRefresh(
+//     id('editor').visibleToUser(true),
+//     "搜索输入框",
+//     8,
+//     this.name
+//   );
+
+//   ele.setText()
+
+// KS.search()
+// id("comment_frame")
+
+// let commentAll = []
+// let commentEles = id("comment_frame").find()
+// for (let c of commentEles) {
+//     if (c.find(id("comment_author_tag")).empty()) {
+//         let comments = c.find(id("comment"))
+//         for (let c of comments) {
+//             commentAll.push(c)
+//         }
+//     } else {
+//         log("包含作者标签")
+//     }
+// }
+
+// for (let c of commentAll) {
+//     log(c.getText())
+// }
+
+// id("tab_filter_image")
+
+// let firstEle = id('comment').findOne()
+// let mainCommentleftPositon = firstEle.bounds().left
+
+// let commentAll = id('comment').find()
+
+// for (let c of commentAll) {
+
+//     // 去除子评论
+//     if (c.bounds().left != mainCommentleftPositon) {
+//         continue
+//     }
+
+//     log("处理 %s",c.getText())
+
+//     // 去除置顶
+//     let top = c.parent().findOne(id("comment_bottom_tag"))
+
+//     log(top)
+
+//     if (top) {
+//         log("包含置顶")
+//         continue
+//     }
+
+//     // 去除作者
+//     let author = c.parent().findOne(id("comment_author_tag"))
+//     if (author) {
+//         log("是作者评论")
+//         continue
+//     }
+
+//     log("---> %s",c.getText())
+
+// }
+
+// log(id('comment').findOne().getText())
+// log(id('comment').findOne().parent().parent().findOne(id("comment_bottom_tag")))
+
+
+// text('EditText')
+
+id('editor').setText('xx')
