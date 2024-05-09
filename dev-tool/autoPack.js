6ac95e59d3edea856d072568e9f7bba309df9d71
auh <21078607+fanlushuai@users.noreply.github.com>
2024-05-10 00:16:59 +0800

    f

--- dev-tool/autoPack.js
+++ dev-tool/autoPack.js
@@ -65,8 +65,8 @@ const Autojsx = {
         (now.getHours() > 9 ? now.getHours() : "0" + now.getHours()) +
         "" +
         (now.getMinutes() > 9 ? now.getMinutes() : "0" + now.getMinutes()) +
-        "" +
-        (now.getSeconds() > 9 ? now.getSeconds() : "0" + now.getSeconds());
+        "";
+      // (now.getSeconds() > 9 ? now.getSeconds() : "0" + now.getSeconds());
       log("设置版本");
       log(verisonName);
       log(versionNum);
